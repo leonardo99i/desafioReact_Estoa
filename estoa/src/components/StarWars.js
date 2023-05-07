ee31467e10a1facb92df6294fe1d255c1558e4dd
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2023-05-07 16:18:08 -0300

    Problema de requisição da API que retornava apenas 10 personagens corrigido

--- estoa/src/components/StarWars.js
+++ estoa/src/components/StarWars.js
@@ -1,15 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+
 import './StarWars.css';
 
 function StarWars() {
   const [characters, setCharacters] = useState([]);
+  const [expandedCharacters, setExpandedCharacters] = useState([]);
 
   useEffect(() => {
     async function fetchCharacters() {
       try {
-        const response = await axios.get('https://swapi.dev/api/people/');
-        setCharacters(response.data.results);
+        let allCharacters = [];
+
+        let response = await axios.get('https://swapi.dev/api/people/');
+        let { results, next } = response.data;
+
+        allCharacters = allCharacters.concat(results);
+
+        while (next) {
+          response = await axios.get(next);
+          results = response.data.results;
+          next = response.data.next;
+
+          allCharacters = allCharacters.concat(results);
+        }
+
+        setCharacters(allCharacters);
       } catch (error) {
         console.log(error);
       }
@@ -18,22 +34,53 @@ function StarWars() {
     fetchCharacters();
   }, []);
 
+  const handleExpandCharacter = (character) => {
+    if (expandedCharacters.includes(character)) {
+      setExpandedCharacters(expandedCharacters.filter((c) => c !== character));
+    } else {
+      setExpandedCharacters([...expandedCharacters, character]);
+    }
+  };
+
   return (
     <div className="star-wars-container">
       <h1>Star Wars Characters</h1>
       <div className="star-wars-cards">
-        {characters.map((character, index) => (
-          <div className="star-wars-card" key={index}>
+        {characters.map((character) => (
+          <div className="star-wars-card" key={character.name}>
             <h2>{character.name}</h2>
             <p>
-              <strong>Height:</strong> {character.height}
-            </p>
-            <p>
-              <strong>Mass:</strong> {character.mass}
+              <strong>Species:</strong> {character.species}
             </p>
             <p>
               <strong>Birth Year:</strong> {character.birth_year}
             </p>
+            {expandedCharacters.includes(character) ? (
+              <div className="expanded-info">
+                <p>
+                  <strong>Eye Color:</strong> {character.eye_color}
+                </p>
+                <p>
+                  <strong>Skin Color:</strong> {character.skin_color}
+                </p>
+                <p>
+                  <strong>Hair Color:</strong> {character.hair_color}
+                </p>
+                <button
+                  className="expand-button"
+                  onClick={() => handleExpandCharacter(character)}
+                >
+                  -
+                </button>
+              </div>
+            ) : (
+              <button
+                className="expand-button"
+                onClick={() => handleExpandCharacter(character)}
+              >
+                +
+              </button>
+            )}
           </div>
         ))}
       </div>
