98b9d3524408450a19a70e2b137301bd9984066c
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2023-05-08 00:20:27 -0300

    ultimos ajustes

--- estoa/src/App.js
+++ estoa/src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import StarWars from './components/StarWars';
 
-
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>STAR WARS API</h1>
-      </header>
       <main>
         <StarWars />
       </main>
